9b8d1e244a9ef78dafd777eb0f55e21dcf580544
Shama Garijo <shamagarijo@gmail.com>
2024-08-05 10:58:36 +0200

    Components created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,27 @@
-// aquí pongo todos mis componentes
-// App.jsx es mi componente madre, desde donde surjen las demás
+import "../scss/App.scss";
+import {useState} from 'react';
+import Main from "./Main";
+import Header from "./Header";
+import ResetButton from "./ResetButton";
+import DiceButton from "./DiceButton";
 
-import '../scss/App.scss'; // hay que escribir la ruta
-import image from '../images/grogu.webp';
 
 
 const App = () => {
+  const [positionGrogu, setpositionGrogu] = useState(0);
 
-}  
-
-
-  return
-      <header>
-      <h1 class="header-title">¡Cuidado con Grogu!</h1>
-      </header>
-      <main class="page">
-      <section class="board">
-        <div class="cell"><div class="grogu">👣</div></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-        <div class="cell"></div>
-      </section>
-      <section>
-        <button class="dice">Lanzar Dado</button>
-        <div class="game-status">En curso</div>
-      </section>
-      <section class="goods-container">
-        <div class="goods-item">🍪</div>
-        <div class="goods-item">🍪</div>
-        <div class="goods-item">🍪</div>
-      </section>
-      <section class="goods-container">
-        <div class="goods-item">🥚</div>
-        <div class="goods-item">🥚</div>
-        <div class="goods-item">🥚</div>
-      </section>
-      <section class="goods-container">
-        <div class="goods-item">🐸</div>
-        <div class="goods-item">🐸</div>
-        <div class="goods-item">🐸</div>
-      </section>
-      <section>
-        <button class="restart-button">Reiniciar Juego</button>
-      </section>
-      </main>
   
+  return (
+    <>
+    <Header/>
+    <Main/>
+    <DiceButton/>
+    <ResetButton/>
+    
 
+
+    </>
+  );
+};
 
 export default App;
--- src/components/Pasos-en-humano.jsx
+++
@@ -1,1 +0,0 @@
-Pasos en humano: 
